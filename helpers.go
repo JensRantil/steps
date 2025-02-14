f823521b0897137810f659ad9bf0423104749f3b
Jens Rantil <jens.rantil@gmail.com>
2025-02-14 14:45:55 +0100

    refactor(helpers): use function type
    ...for increased readability.

--- helpers.go
+++ helpers.go
@@ -5,7 +5,7 @@ import "time"
 // Ticker schedules an event to run at a regular interval. For more complex cronjobs etc., have a look at something like [1].
 //
 // [1]: https://pkg.go.dev/github.com/robfig/cron#Schedule
-func Ticker(sim *Simulation, start time.Time, duration time.Duration, f func(s *Simulation)) {
+func Ticker(sim *Simulation, start time.Time, duration time.Duration, f Action) {
 	var nextRun func(s *Simulation)
 	nextRun = func(s *Simulation) {
 		f(s)
